e47322445f2fbbff6f03c68d13e93e3aa406b791
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-27 10:07:30 +0300

    fix: edit line-length rule

--- .markdownlintrc.js
+++ .markdownlintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   'code-block-style': { style: 'fenced' },
   'code-fence-style': { style: 'backtick' },
   'first-line-heading': false,
-  'line-length': { strict: false },
+  'line-length': {
+    strict: false,
+    headings: false,
+    code_blocks: false,
+    tables: false,
+  },
   'no-inline-html': false,
   'ol-prefix': { style: 'ordered' },
   'ul-style': { style: 'dash' },
