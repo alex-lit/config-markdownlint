09207ab5dc3b6da80d01c0d3c8922e4abe876762
Alexey Litovchenko <alex.lit@outlook.com>
2021-05-15 01:06:39 +0300

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
+const SPELLCHECK_RULES = require('@alexlit/config-eslint/plugins/spellcheck')
+  .rules['spellcheck/spell-checker'][1];
+
 module.exports = {
   extends: ['./node_modules/@alexlit/lint-kit/.eslintrc.js'],
+
+  rules: {
+    'spellcheck/spell-checker': [
+      'warn',
+      {
+        ...SPELLCHECK_RULES,
+
+        skipWords: [...SPELLCHECK_RULES.skipWords, 'maxerr'],
+      },
+    ],
+  },
 };
